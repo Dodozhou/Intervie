abd1037920dd06b1b3cae484962dd3b9f811c0cf
youzeliang <youzel@126.com>
2017-12-17 17:33:15 +0800

    base

--- src/test.java
+++ src/test.java
@@ -2,5 +2,13 @@
  * Created by youzeliang on 2017/12/15.
  */
 public class test {
+    String name="1";
 
+    public static void main(String[] args) {
+
+
+    }
+    void talk(){
+        System.out.println("我叫"+this.name);
+    }
 }
